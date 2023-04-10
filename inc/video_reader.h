6994e94434af00e42a5ae8adca87792f9b5c587b
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2023-04-11 01:10:00 +0530

    Added libs and functions related to video rendering

---
+++ inc/video_reader.h
@@ -0,0 +1,26 @@
+#pragma once
+extern "C"
+{
+    #include<libavcodec/avcodec.h>
+    #include<libavformat/avformat.h>
+    #include<libswscale/swscale.h>
+    #include<inttypes.h>
+}
+
+
+struct VideoReaderState
+{
+    int width, height;
+    uint8_t* frame_buffer;
+    AVFormatContext* avFormatContext;
+    AVCodecContext* avCodecContext;
+    int video_stream_index;
+    AVFrame* avFrame;
+    AVPacket* avPacket;
+    SwsContext* swsScaleContext;
+};
+
+
+bool video_reader_open(VideoReaderState*, const char*);
+bool video_reader_read_frame(VideoReaderState* , uint8_t*);
+bool video_reader_close(VideoReaderState*);
