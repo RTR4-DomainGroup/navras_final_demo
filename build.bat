1339814c78fcf44129302c8fed2b7c9f31da4f2e
ShrutiKulkarni03 <shru3k@gmail.com>
2023-04-28 00:40:39 +0530

    paths updated

--- build.bat
+++ build.bat
@@ -6,13 +6,13 @@ cl.exe /c /EHsc /I C:\glew\include /I ext\ /I ext\Assimp\include\ src\*.cpp src\
 
 rc.exe res\resources.rc
 
-link.exe /OUT:"Navras.exe" *.obj /Libpath:C:\glew\lib\Release\x64 /Libpath:ext\ res\resources.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS ^
+link.exe /OUT:"Navras.exe" *.obj /Libpath:C:\glew\lib\Release\x64 /Libpath:ext\ /Libpath:ext\Assimp\lib\ res\resources.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS ^
 /NODEFAULTLIB:library MSVCRTD.lib /NODEFAULTLIB:library LIBCMT.lib 
 
 del *.obj
 
 del res\resources.res
 
-set PATH=%PATH%;.\ext\AL\bin\
+set PATH=%PATH%;.\ext\AL\bin\;.\ext\Assimp\bin\
 
 Navras.exe
