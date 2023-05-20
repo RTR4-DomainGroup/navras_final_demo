5204829b253485227257682447305775b6640443
ShrutiKulkarni03 <shru3k@gmail.com>
2023-05-21 04:49:17 +0530

    man test

--- src/scenes/scenePlaceHolderIndoor.cpp
+++ src/scenes/scenePlaceHolderIndoor.cpp
@@ -103,7 +103,7 @@ int initializeScene_PlaceHolderIndoor(void)
 #ifdef ENABLE_DYNAMIC_MODELS
 	//loadDynamicModel("res/models/skeleton/sadWalk.fbx", &skeletonModel_in);
 	//loadDynamicModel("res/models/exo/Walking.dae", &skeletonModel_in);
-	loadDynamicModel("res/models/exo1/Exo.fbx", &skeletonModel_in);
+	loadDynamicModel("res/models/malcolm/malcolmWalk.fbx", &skeletonModel_in);
 #endif
 
 
