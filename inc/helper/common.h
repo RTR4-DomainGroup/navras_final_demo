1fcd14d0a0a76911f063f90fc667da8acb7b72e2
Prashant <adlinge.pa@gmail.com>
2023-05-23 19:51:15 +0530

    uncommented heaaders

--- inc/helper/common.h
+++ inc/helper/common.h
@@ -8,11 +8,11 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
-// #include <glm/gtx/quaternion.hpp>
-// #include "../../inc/helper/assimp_glm_helpers.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include "../../inc/helper/assimp_glm_helpers.h"
 
 #include "resources.h"
 #include "vmath.h"
