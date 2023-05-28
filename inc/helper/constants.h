14ae676ad508e4fbbd106b9e2ba6f69d37506dad
ShrutiKulkarni03 <shru3k@gmail.com>
2023-05-29 00:50:51 +0530

    shanta incomplete

--- inc/helper/constants.h
+++ inc/helper/constants.h
@@ -105,6 +105,6 @@ static const char *szAudios[] = {
 //#define CURRENT_SCENE SCENE10_ADBHUT_RAS          // Adbhut Ras
 //#define CURRENT_SCENE SCENE11_SHRINGAR_RAS        // Shringar Ras
 //#define CURRENT_SCENE SCENE12_HASYA_RAS           // Hasya Ras
-#define CURRENT_SCENE SCENE13_SHANT_RAS           // Shant Ras
+//#define CURRENT_SCENE SCENE13_SHANT_RAS           // Shant Ras
 //#define CURRENT_SCENE SCENE14_PARTICLE            // Particle Simulation
 //#define CURRENT_SCENE SCENE15_END_CREDIS          // End Credits
