eae570c8416f64e41c70c082a4d81fda083da813
ShrutiKulkarni03 <shru3k@gmail.com>
2023-05-29 17:58:16 +0530

    version 1 in integration

--- inc/helper/constants.h
+++ inc/helper/constants.h
@@ -92,10 +92,10 @@ static const char *szAudios[] = {
 
 //#define CURRENT_SCENE SCENE00_AMC_BANNER          // Loading AMC banner 
 //#define CURRENT_SCENE SCENE01_DOMAIN_LOGO         // Domain Logo animation
-//#define CURRENT_SCENE SCENE02_EARTH_AND_SPACE     // Earth and Space
+#define CURRENT_SCENE SCENE02_EARTH_AND_SPACE     // Earth and Space
 //#define CURRENT_SCENE SCENE03_LUCY                // Lucy
 //#define CURRENT_SCENE SCENE04_INTRO               // Intro 
-#define CURRENT_SCENE SCENE05_KARUN_RAS           // Karun Ras 
+//#define CURRENT_SCENE SCENE05_KARUN_RAS           // Karun Ras 
 //#define CURRENT_SCENE SCENE06_BHAYANK_RAS         // Bhayanak Ras
 //#define CURRENT_SCENE SCENE07_RAUDRA_RAS          // Raudra Ras
 //#define CURRENT_SCENE SCENE08_BIBHATSA_RAS        // Bibhatsa Ras
