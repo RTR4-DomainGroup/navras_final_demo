87917b3cddae1172e71f770c798423ab24b3ae70
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2023-06-15 04:51:51 +0530

    Removed multi-threading and added endcredits

--- inc/helper/constants.h
+++ inc/helper/constants.h
@@ -73,7 +73,7 @@ static const char *szAudios[] = {
     "Scene12_HasyaRas_40_seconds.wav",
     "Scene13_ShantRas_40_seconds.wav",
     "Scene14_Particle_40_seconds.wav",
-    "audio1.wav",
+    "EndCredits.wav",
     "audio3.wav",
 };
 
@@ -131,7 +131,7 @@ static const char *szAudios[] = {
 //#define CURRENT_SCENE SCENE12_HASYA_RAS           // Hasya Ras
 //#define CURRENT_SCENE SCENE13_SHANT_RAS           // Shant Ras
 //#define CURRENT_SCENE SCENE14_PARTICLE            // Particle Simulation
-//#define CURRENT_SCENE SCENE15_END_CREDIS          // End Credits
+//#define CURRENT_SCENE SCENE15_END_CREDITS          // End Credits
 
 #else
 
